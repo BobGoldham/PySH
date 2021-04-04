bca967f6221bd5d1d5af1aa3744d576910130b7b
Anton Kossler <a.kossler@digipen.edu>
2021-04-03 17:23:27 -0700

    "Fixed a bug where job suspension called a removed function"

--- main.py
+++ main.py
@@ -55,9 +55,9 @@ class Signal_TSTOP(Exception):
     pass
 
 def handle_sigtstp(signum, frame):
-    if frame.f_code == execute:
+    if frame.f_code == execute_command:
         job = frame.f_locals["process"]
-        os.kill(job.pid, signal.SIGSTOP)
+        os.kill(job.pid, signal.SIGTSTP)
         jobs.put(job)
     raise Signal_TSTOP
 
@@ -71,16 +71,27 @@ def handle_builtins(cmd):
         return True
     return False
 
-def execute(cmd):
+def execute_command(cmd, stdin=None, stdout=None):
+    args = cmd.split(' ')
+    i = 0
+    while i < len(args):
+        if args[i][0] == '"':
+            while not args[i][-1] == '"':
+                args[i] = ' '.join([args[i], args[i+1]])
+                args = args[:i+1] + args[i+2:] if len(args) > i+2 else args[:i+1]
+        i = i + 1
+    return subprocess.Popen(args, stdin=stdin, stdout=stdout)
+
+def parse_command(cmd):
     subcommands = cmd.split('|')
     try:
         tmppipe = sys.stdout
         for i in range(0, len(subcommands)-1):
             cmd = subcommands[i].strip()
-            process = subprocess.Popen(cmd.split(' '), stdin=tmppipe, stdout=subprocess.PIPE)
+            process = execute_command(cmd, stdin=tmppipe, stdout=subprocess.PIPE)
             tmppipe = process.stdout
         cmd = subcommands[-1].strip()
-        process = subprocess.Popen(cmd.split(' '), stdin=tmppipe)
+        process = execute_command(cmd, stdin=tmppipe)
         process.wait()
     except KeyboardInterrupt:
         print("")
@@ -112,7 +123,7 @@ def prompt():
             exec(cmd)
         except (NameError,SyntaxError) as e:
             try:
-                execute(cmd)
+                parse_command(cmd)
             except FileNotFoundError:
                 print("That was neither a valid python snippet, nor a valid command.")
                 print(e)
